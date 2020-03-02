328aab2b9dc8a1793d4e4e8da0466621e0ba56a4
julianhallojack <julian@hallojack.com>
2020-03-02 14:14:53 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mia-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'prueba-miaguila';
+}
